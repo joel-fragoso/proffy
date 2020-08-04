1f609e5f34279354c4be03c473293d497b599a2e
Joel Fragoso <joelfragoso85@icloud.com>
2020-08-04 14:14:27 -0300

    fix: width form and page-header

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -10,17 +10,19 @@ const TeacherList: React.FC = () => {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search">
-          <div className="search__input">
-            <label htmlFor="search__input-subject">Matéria</label>
-            <input type="text" id="search__input-subject" />
-          </div>
-          <div className="search__input">
-            <label htmlFor="search__input-week-day">Dia da semana</label>
-            <input type="text" id="search__input-week-day" />
-          </div>
-          <div className="search__input">
-            <label htmlFor="search__input-time">Horas</label>
-            <input type="text" id="search__input-time" />
+          <div className="search__container">
+            <div className="search__input">
+              <label htmlFor="search__input-subject">Matéria</label>
+              <input type="text" id="search__input-subject" />
+            </div>
+            <div className="search__input">
+              <label htmlFor="search__input-week-day">Dia da semana</label>
+              <input type="text" id="search__input-week-day" />
+            </div>
+            <div className="search__input">
+              <label htmlFor="search__input-time">Horas</label>
+              <input type="text" id="search__input-time" />
+            </div>
           </div>
         </form>
       </PageHeader>
